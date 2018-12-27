6c2353e0540f917a9b9ee726a2266cbea52da44a
Endel Dreyer <endel.dreyer@gmail.com>
2018-12-26 22:36:13 -0200

    test initial values

--- test/StateTest.ts
+++ test/StateTest.ts
@@ -1,30 +1,21 @@
 import * as msgpack from "notepack.io";
 import * as assert from "assert";
 import { State, Player } from '../example/State';
+import { Sync, sync } from "../src/annotations";
 
 describe("State", () => {
-    // describe("data types", () => {
-    //     it("string", () => {
-    //         const state = new State();
-    //         state.fieldString = "Hello world!";
-    //         // assert.deepEqual(state.encode(), [ 172, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33 ]);
-    //     })
+    describe("declaration", () => {
+        it("should allow to define default values", () => {
+            class DataObject extends Sync {
+                @sync("string")
+                stringValue = "initial value";
+            }
 
-    //     it("int", () => {
-    //         const state = new State();
-    //         state.fieldNumber = 5;
-    //         assert.equal(state.fieldNumber, 5);
-
-    //         state.fieldNumber += 1;
-    //         assert.equal(state.fieldNumber, 6);
-    //     });
-
-    //     it("float", () => {
-    //         const state = new State();
-    //         state.fieldNumber = 42.22;
-    //         assert.equal(state.fieldNumber, 42.22);
-    //     });
-    // });
+            let data = new DataObject();
+            assert.equal(data.stringValue, "initial value");
+            assert.deepEqual((DataObject as any)._schema, { stringValue: 'string' });
+        });
+    });
 
     describe("patching", () => {
         it("should encode changed values", () => {
