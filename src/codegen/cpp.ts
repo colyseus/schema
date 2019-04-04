066e0de558fac5bb1ecf8cbb7567a2c62fffbf7c
Endel Dreyer <endel.dreyer@gmail.com>
2019-04-04 11:51:34 -0300

    cpp codegen: support inheritance

--- src/codegen/cpp.ts
+++ src/codegen/cpp.ts
@@ -44,20 +44,42 @@ const capitalize = (s) => {
 export function generate (classes: Class[], args: any): File[] {
     return classes.map(klass => ({
         name: klass.name + ".hpp",
-        content: generateClass(klass, args.namespace)
+        content: generateClass(klass, args.namespace, classes)
     }));
 }
 
-function generateClass(klass: Class, namespace: string) {
+function generateClass(klass: Class, namespace: string, allClasses: Class[]) {
     const propertiesPerType: {[type: string]: Property[]} = {};
+    const allRefs: Property[] = [];
     klass.properties.forEach(property => {
-        if (!propertiesPerType[property.type]) {
-            propertiesPerType[property.type] = [];
+        let type = property.type;
+
+        if (!propertiesPerType[type]) {
+            propertiesPerType[type] = [];
         }
-        propertiesPerType[property.type].push(property);
+
+        propertiesPerType[type].push(property);
+
+        // keep all refs list
+        if ((type === "ref" || type === "array" || type === "map")) {
+            allRefs.push(property);
+        }
     });
 
+    const allProperties = getAllProperties(klass, allClasses);
+    const createInstanceMethod = (allRefs.length === 0) ? "" : 
+    `\tSchema* createInstance(std::type_index type) {
+\t\t${generateFieldIfElseChain(allRefs, 
+    (property) => `type == typeof(${property.childType})`,
+    (property) => `return this->${property.name};`,
+    (property) => property.childType !== undefined)}
+\t\treturn ${klass.extends}::createInstance(field);
+\t}`;
+
     return `${getCommentHeader()}
+#ifndef __SCHEMA_CODEGEN_${klass.name.toUpperCase()}_H__
+#define __SCHEMA_CODEGEN_${klass.name.toUpperCase()}_H__ 1
+
 using namespace colyseus::schema;
 
 ${namespace ? `namespace ${namespace} {` : ""}
@@ -65,12 +87,22 @@ class ${klass.name} : public ${klass.extends} {
 public:
 ${klass.properties.map(prop => generateProperty(prop)).join("\n")}
 
+\t${klass.name}() {
+\t\tthis->_indexes = ${generateAllIndexes(allProperties)};
+\t\tthis->_types = ${generateAllTypes(allProperties)};
+\t\tthis->_childTypes = ${generateAllChildTypes(allProperties)};
+\t}
+
 protected:
 ${Object.keys(propertiesPerType).map(type => 
-    generateGettersAndSetters(type, propertiesPerType[type])).
+    generateGettersAndSetters(klass, type, propertiesPerType[type])).
     join("\n")}
+
+${createInstanceMethod}
 }
 ${namespace ? "}" : ""}
+
+#endif
 `;
 }
 
@@ -99,7 +131,6 @@ function generateProperty(prop: Property) {
             initializer = `new ${langType}()`;
         }
 
-
     } else {
         langType = typeMaps[prop.type];
         initializer = typeInitializer[prop.type];
@@ -110,42 +141,87 @@ function generateProperty(prop: Property) {
     return `\t${property} = ${initializer};`
 }
 
-function generateGettersAndSetters(type: string, properties: Property[]) {
+function generateGettersAndSetters(klass: Class, type: string, properties: Property[]) {
+    let langType = typeMaps[type];
+    const methodName = `get${capitalize(type)}`;
+
     if (type === "ref") {
+        langType = "Schema*";
 
     } else if (type === "array") {
+        langType = `ArraySchema<T>*`;
 
     } else if (type === "map") {
+        langType = `MapSchema<T>*`;
+    }
 
-    } else {
-        const langType = typeMaps[type];
-        return `\t${langType} get${capitalize(type)}(string field)
+    return `\t${langType} ${methodName}(string field)
 \t{
-\t\t${generateFieldIfElseChain(properties, (currentProperty) => (
-    `return this->${currentProperty.name};`
-))}
-\t\treturn ${typeInitializer[type]};
+\t\t${generateFieldIfElseChain(properties, 
+    (property) => `field == "${property.name}"`,
+    (property) => `return this->${property.name};`)}
+\t\treturn ${klass.extends}::${methodName}(field);
 \t}
 
 \tvoid set${capitalize(type)}(string field, ${langType} value)
 \t{
-\t\t${generateFieldIfElseChain(properties, (currentProperty) => (
-    `this->${currentProperty.name} = value;`
-))}
+\t\t${generateFieldIfElseChain(properties,
+    (property) => `field == "${property.name}"`,
+    (property) => `this->${property.name} = ${((property.childType) ? `(${langType})` : "")}value;`)}
 \t}`;
-    }
 }
 
-function generateFieldIfElseChain(properties: Property[], callback: (currentProperty: Property) => string) {
+function generateFieldIfElseChain(
+    properties: Property[],
+    ifCallback: (property: Property) => string,
+    callback: (property: Property) => string,
+    filter: (property: Property) => boolean = (_) => true
+) {
     let chain = "";
 
-    properties.forEach((property, i) => {
+    properties.filter(filter).forEach((property, i) => {
         if (i === 0) { chain += "if " } else { chain += " else if " }
-        chain += `(field == "${property.name}")
+        chain += `(${ifCallback(property)})
 \t\t{
 \t\t\t${callback(property)}\n
 \t\t}`
     });
 
     return chain;
+}
+
+function generateAllIndexes(properties: Property[]) {
+    return `{${properties.map((property, i) => `{${i}, "${property.name}"}`).join(", ")}}`
+
+}
+
+function generateAllTypes(properties: Property[]) {
+    return `{${properties.map((property, i) => `{${i}, "${property.type}"}`).join(", ")}}`
+}
+
+function generateAllChildTypes(properties: Property[]) {
+    return `{${properties.map((property, i) => {
+        if (property.childType) {
+            return `{${i}, typeid(${property.childType})}`
+        } else {
+            return null;
+        }
+    }).filter(r => r !== null).join(", ")}}`
+}
+
+function getAllProperties (klass: Class, allClasses: Class[]) {
+    let properties: Property[] = [];
+
+    let currentClass = klass;
+    let inheritanceTree = [currentClass];
+    while (currentClass.extends !== "Schema") {
+        currentClass = allClasses.find(klass => klass.name == currentClass.extends);
+        inheritanceTree.push(currentClass);
+    }
+
+    inheritanceTree.reverse().forEach((klass) => {
+        properties = properties.concat(klass.properties);
+    });
+
+    return properties;
 }
