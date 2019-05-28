f0fb10794ba9eeddca9592da0a5e3885f9876061
Endel Dreyer <endel.dreyer@gmail.com>
2019-05-28 12:29:11 -0300

    update test-external for ArraySchema and Inherited types.

--- test-external/ArraySchemaTypes.ts
+++ test-external/ArraySchemaTypes.ts
@@ -8,23 +8,35 @@ class IAmAChild extends Schema {
 class ArraySchemaTypes extends Schema {
   @type([IAmAChild]) arrayOfSchemas = new ArraySchema<IAmAChild>();
   @type(["number"]) arrayOfNumbers = new ArraySchema<number>();
+  @type(["string"]) arrayOfStrings = new ArraySchema<string>();
+  @type(["int32"]) arrayOfInt32 = new ArraySchema<number>();
 }
 
 const state = new ArraySchemaTypes();
+
+const item1 = new IAmAChild();
+item1.x = 100;
+item1.y = -100;
+state.arrayOfSchemas.push(item1);
+
+const item2 = new IAmAChild();
+item2.x = 100;
+item2.y = -100;
+state.arrayOfSchemas.push(item2);
+
 state.arrayOfNumbers.push(0);
 state.arrayOfNumbers.push(10);
 state.arrayOfNumbers.push(20);
 state.arrayOfNumbers.push(30);
 
-let item1 = new IAmAChild();
-item1.x = 100;
-item1.y = -100;
-state.arrayOfSchemas.push(item1);
+state.arrayOfStrings.push("one");
+state.arrayOfStrings.push("two");
+state.arrayOfStrings.push("three");
 
-let item2 = new IAmAChild();
-item2.x = 100;
-item2.y = -100;
-state.arrayOfSchemas.push(item2);
+state.arrayOfInt32.push(1000);
+state.arrayOfInt32.push(2000);
+state.arrayOfInt32.push(-3000);
+
 
 const bytes = Array.from(Uint8Array.from(Buffer.from( state.encode() )));
 
