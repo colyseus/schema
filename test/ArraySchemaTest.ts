4068f462e92729decf77357bb416443c9fd70e97
Darek Zieliński <dariusz.zielinski@dazn.com>
2019-07-14 15:13:55 +0200

    more tests to help with colyseus/colyseus.js#53 also, it helps to ignore /lib/ and keep package-lock.json in the repo ;)

--- test/ArraySchemaTest.ts
+++ test/ArraySchemaTest.ts
@@ -55,7 +55,108 @@ describe("ArraySchema", () => {
         }
     });
 
-    xit("should allow to transfer object between ArraySchema", () => {
+    xit("updates all items custom `idx` after removing middle item", () => {
+        /**
+         * In this scenario, after splicing middle item, I'm updating
+         * each item's `idx` property, to reflect its current "index".
+         * After remiving "Item 3", items 4 and 5 would get their
+         * `idx` updated. Rest of properties should remain unchanged.
+         */
+        const stringifyItem = i => `[${i.idx}] ${i.name} (${i.id})`;
+
+        class Item extends Schema {
+            @type("uint8") id: number;
+            @type("uint8") idx: number;
+            @type("string") name: string;
+            constructor(name, idx) {
+                super();
+                this.idx = idx
+                this.name = name;
+                this.id = Math.round(Math.random() * 250);
+            }
+        }
+        class Player extends Schema {
+            @type([Item]) items = new ArraySchema<Item>();
+        }
+        class State extends Schema {
+            @type(Player) player1 = new Player();
+        }
+
+        const state = new State();
+        const decodedState = new State();
+
+        state.player1.items.push(new Item("Item 1", 0));
+        state.player1.items.push(new Item("Item 2", 1));
+        state.player1.items.push(new Item("Item 3", 2));
+        state.player1.items.push(new Item("Item 4", 3));
+        state.player1.items.push(new Item("Item 5", 4));
+        decodedState.decode(state.encodeAll());
+        assert.equal(decodedState.player1.items.length, 5);
+
+        // Remove one item
+        state.player1.items.splice(2, 1);
+        decodedState.decode(state.encode());
+        
+        assert.equal(decodedState.player1.items.length, 4);
+        
+        // Update `idx` of each item
+        state.player1.items.forEach((item, idx) => item.idx = idx);
+        decodedState.decode(state.encode());
+
+        const resultPreEncoding = state.player1.items
+            .map(stringifyItem);
+        const resultPostEncoding = decodedState.player1.items
+            .map(stringifyItem);
+
+        // Ensure all data is perserved and `idx` is updated for each item
+        assert.equal(
+            resultPostEncoding,
+            resultPreEncoding,
+            `There's a difference between state and decoded state on some items`
+        );
+    })
+
+    xit("updates an item after removing another", () => {
+        class Item extends Schema {
+            @type("string") name: string;
+            constructor(name) {
+                super();
+                this.name = name;
+            }
+        }
+        class Player extends Schema {
+            @type([Item]) items = new ArraySchema<Item>();
+        }
+        class State extends Schema {
+            @type(Player) player = new Player();
+        }
+
+        const state = new State();
+        const decodedState = new State();
+
+        state.player.items.push(new Item("Item 1"));
+        state.player.items.push(new Item("Item 2"));
+        state.player.items.push(new Item("Item 3"));
+        decodedState.decode(state.encodeAll());
+
+        // Remove Item 2
+        const [ removedItem ] = state.player.items.splice(1, 1);
+        assert.equal(removedItem.name, "Item 2");
+        // If you disable below decoding, the later assertions pass (?!)
+        decodedState.decode(state.encode());
+        
+        // Update `name` of remaining item
+        const preEncoding = state.player.items[1].name = "Item 3 changed!";
+        decodedState.decode(state.encode());
+
+        assert.equal(
+            decodedState.player.items[1].name,
+            preEncoding,
+            `new name of Item 3 was not reflected during recent encoding/decoding.`
+        );
+    })
+
+    it("should allow to transfer object between ArraySchema", () => {
         class Item extends Schema {
             @type("uint8") id: number;
             @type("string") name: string;
