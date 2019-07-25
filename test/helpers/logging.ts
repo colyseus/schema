770f43288e51a437e112353080ec698eb05e7d62
Darek Zieliński <dariusz.zielinski@dazn.com>
2019-07-25 20:35:07 +0200

    no need for downlevelIteration anywhere now.

--- test/helpers/logging.ts
+++ test/helpers/logging.ts
@@ -3,8 +3,11 @@ import { ChangeTree } from "../../src/ChangeTree";
 export const logChangeTree = (tree: ChangeTree) => {
     if (!tree) return '{empty}'
     const logMap = (map: Map<any, string | number | symbol>) => {
-        return [...map.entries()]
-            .map(([key, value]) => `Item"${key.name}".${key.x} => ${String(value)}`)
+        let result = []
+        map.forEach(
+            (value, key) => result.push(`Item"${key.name}".${key.x} => ${String(value)}`)
+        )
+        return result
     }
     return `
 {
