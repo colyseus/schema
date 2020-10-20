a67e44fc89e6375ebbc8c5550b22493b974c337a
Endel Dreyer <endel.dreyer@gmail.com>
2020-10-20 14:06:49 -0300

    schema-codegen: fixes haxe version to meet latest version

--- src/codegen/languages/haxe.ts
+++ src/codegen/languages/haxe.ts
@@ -60,7 +60,8 @@ function generateClass(klass: Class, namespace: string, allClasses: Class[]) {
     return `${getCommentHeader()}
 
 ${namespace ? `package ${namespace};` : ""}
-import io.colyseus.serializer.schema.*;
+import io.colyseus.serializer.schema.Schema;
+import io.colyseus.serializer.schema.types.*;
 
 class ${klass.name} extends ${klass.extends} {
 ${klass.properties.map(prop => generateProperty(prop)).join("\n")}
