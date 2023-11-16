colyseus/schema

commit 01615aff2129bc4fe9d58f5c597c6046c2d2f22f

improve toJSON() return type.

src/types/HelperTypes.ts
@@ -1,34 +1,18 @@
-type Bool = 'true' | 'false'
-type Key = string | number | symbol;
+import { ArraySchema } from "./ArraySchema";
+import { MapSchema } from "./MapSchema";
 
-type Not<X extends Bool> = {
-    true: 'false',
-    false: 'true'
-}[X]
-
-type HaveIntersection<S1 extends string, S2 extends string> = (
-    { [K in S1]: 'true' } &
-    { [key: string]: 'false' }
-)[S2]
-
-type IsNeverWorker<S extends Key> = (
-    { [K in S]: 'false' } &
-    { [key: string]: 'true' }
-)[S]
-
-// Worker needed because of https://github.com/Microsoft/TypeScript/issues/18118
-type IsNever<T extends Key> = Not<HaveIntersection<IsNeverWorker<T>, 'false'>>
-
-type IsFunction<T> = IsNever<keyof T>
-
-export type NonFunctionProps<T> = {
-    [K in keyof T]: {
-        'false': K,
-        'true': never
-    }[IsFunction<T[K]>]
-}[keyof T];
+export type NonFunctionProps<T> = Omit<T, {
+    [K in keyof T]: T[K] extends Function ? K : never;
+}[keyof T]>;
 
 export type NonFunctionPropNames<T> = {
     [K in keyof T]: T[K] extends Function ? never : K
 }[keyof T];
 
+export type ToJSON<T> = Partial<NonFunctionProps<{
+    [K in keyof T]: T[K] extends MapSchema<infer U>
+        ? Record<string, U>
+        : T[K] extends ArraySchema<infer U>
+            ? U[]
+            : T[K]
+}>>;

src/Schema.ts
@@ -11,7 +11,7 @@ import { CollectionSchema } from './types/CollectionSchema';
 import { SetSchema } from './types/SetSchema';
 
 import { ChangeTree, Ref, ChangeOperation } from "./changes/ChangeTree";
-import { NonFunctionPropNames } from './types/HelperTypes';
+import { NonFunctionPropNames, ToJSON } from './types/HelperTypes';
 import { ClientState } from './filters';
 import { getType } from './types/typeRegistry';
 import { ReferenceTracker } from './changes/ReferenceTracker';
@@ -184,7 +184,7 @@ export abstract class Schema {
     }
 
     public assign(
-        props: { [prop in NonFunctionPropNames<this>]?: this[prop] }
+        props: { [prop in NonFunctionPropNames<this>]?: this[prop] } | ToJSON<this>,
     ) {
         Object.assign(this, props);
         return this;
@@ -898,7 +898,7 @@ export abstract class Schema {
         const schema = this._definition.schema;
         const deprecated = this._definition.deprecated;
 
-        const obj = {}
+        const obj: ToJSON<typeof this> = {};
         for (let field in schema) {
             if (!deprecated[field] && this[field] !== null && typeof (this[field]) !== "undefined") {
                 obj[field] = (typeof (this[field]['toJSON']) === "function")

test/SchemaTest.ts
@@ -973,7 +973,7 @@ describe("Schema Usage", () => {
 
             state.value.set('k1', 1);
             const firstEncoded = state.encodeAll();
-            
+
             const decodedState1 = new TestMapSchema();
             decodedState1.decode(firstEncoded);
             assert.deepStrictEqual(decodedState1.toJSON(), state.toJSON(), `decodedState1.toJSON() EQ state.toJSON()`);

commit 372a89807c7ebcdab9690ef753dd02c2e6fa5dff

TypeScript: fixes ToJSON return type. Regression from 01615aff2129bc4fe9d58f5c597c6046c2d2f22f

src/types/HelperTypes.ts
@@ -12,7 +12,9 @@ export type NonFunctionPropNames<T> = {
 export type ToJSON<T> = NonFunctionProps<{
     [K in keyof T]: T[K] extends MapSchema<infer U>
         ? Record<string, U>
-        : T[K] extends ArraySchema<infer U>
-            ? U[]
-            : T[K]
+        : T[K] extends Map<string, infer U>
+            ? Record<string, U>
+            : T[K] extends ArraySchema<infer U>
+                ? U[]
+                : T[K]
 }>;

test/TypeScriptTypes.test.ts
@@ -16,6 +16,30 @@ describe("TypeScript Types", () => {
             orderPriority: null,
         }));
         state.encodeAll();
-        console.log("DONE!");
+        assert.ok(true);
     });
+
+    describe("complex declaration scenarios", () => {
+        it("implements / extends without conflicts", () => {
+            // Defines a generic schema
+            interface SchemaInterface extends Schema {
+                players: Map<string, string>;
+                items: string[];
+            }
+
+            // Implements the above interface
+            // MapSchema is compatible with Map
+            class SchemaInterfaceImpl extends Schema implements SchemaInterface {
+                players: MapSchema<string>;
+                items: ArraySchema<string>;
+            }
+
+            // Uses the schema interface
+            abstract class AbstractRoom<T extends SchemaInterface> { }
+
+            // Uses the schema implementation
+            class AbstractRoomImpl extends AbstractRoom<SchemaInterfaceImpl> { }
+        });
+
+    })
 });