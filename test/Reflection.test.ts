08f8ffe124d8fc87dde38b09d7958ca6e8ecc3c4
Endel Dreyer <endel.dreyer@gmail.com>
2024-05-24 20:58:21 -0300

    Reflection: uncomment test

--- test/Reflection.test.ts
+++ test/Reflection.test.ts
@@ -43,33 +43,31 @@ describe("Reflection", () => {
         const reflected = new Reflection();
         reflected.decode(Reflection.encode(state));
 
-        console.log(reflected.toJSON());
-
-        // assert.deepStrictEqual(
-        //     reflected.toJSON(),
-        //     {
-        //         types: [
-        //             {
-        //                 id: 0,
-        //                 fields: [
-        //                     { name: 'fieldString', type: 'string' },
-        //                     { name: 'fieldNumber', type: 'number' },
-        //                     { name: 'player', type: 'ref', referencedType: 1 },
-        //                     { name: 'arrayOfPlayers', type: 'array', referencedType: 1 },
-        //                     { name: 'mapOfPlayers', type: 'map', referencedType: 1 }
-        //                 ]
-        //             },
-        //             {
-        //                 id: 1,
-        //                 fields: [
-        //                     { name: 'name', type: 'string' },
-        //                     { name: 'x', type: 'number' },
-        //                     { name: 'y', type: 'number' }
-        //                 ]
-        //             }
-        //         ]
-        //     }
-        // );
+        assert.deepStrictEqual(
+            reflected.toJSON(),
+            {
+                types: [
+                    {
+                        id: 0,
+                        fields: [
+                            { name: 'fieldString', type: 'string' },
+                            { name: 'fieldNumber', type: 'number' },
+                            { name: 'player', type: 'ref', referencedType: 1 },
+                            { name: 'arrayOfPlayers', type: 'array', referencedType: 1 },
+                            { name: 'mapOfPlayers', type: 'map', referencedType: 1 }
+                        ]
+                    },
+                    {
+                        id: 1,
+                        fields: [
+                            { name: 'name', type: 'string' },
+                            { name: 'x', type: 'number' },
+                            { name: 'y', type: 'number' }
+                        ]
+                    }
+                ]
+            }
+        );
     });
 
     it("reflected fields must initialize as undefined", () => {
