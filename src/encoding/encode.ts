18ed4a6bfbaa40996fc3052ffeeaa7e2a69588ef
Nulled <63370961+Nul-led@users.noreply.github.com>
2025-01-10 19:45:38 +0100

    feat: add float32 checks for var len serialization

--- src/encoding/encode.ts
+++ src/encoding/encode.ts
@@ -215,17 +215,19 @@ export function number(bytes: BufferLike, value: number, it: Iterator) {
     return number(bytes, (value > 0) ? Number.MAX_SAFE_INTEGER : -Number.MAX_SAFE_INTEGER, it);
 
   } else if (value !== (value|0)) {
+    if (Math.abs(value) < 3.4028235e+38) { // range check
+        _float32[0] = value;
+        if (Math.abs(_float32[0] - v) < 1e-4) { // precision check; adjust 1e-n (n = precision) to in-/decrease acceptable precision loss
+            // now we know value is in range for f32 and has acceptable precision for f32
+            bytes[it.offset++] = 0xca;
+            writeFloat32(bytes, value);
+            return 5;
+        }
+    }
+      
     bytes[it.offset++] = 0xcb;
     writeFloat64(bytes, value, it);
     return 9;
-
-    // TODO: encode float 32?
-    // is it possible to differentiate between float32 / float64 here?
-
-    // // float 32
-    // bytes.push(0xca);
-    // writeFloat32(bytes, value);
-    // return 5;
   }
 
   if (value >= 0) {
