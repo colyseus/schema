bcb059c7fafca5e00047de51bbb2d150b5317356
Endel Dreyer <endel.dreyer@gmail.com>
2025-02-21 19:27:45 -0300

    fix ArraySchema.every(), avoid @ts-ignore

--- src/types/custom/ArraySchema.ts
+++ src/types/custom/ArraySchema.ts
@@ -504,7 +504,8 @@ export class ArraySchema<V = any> implements Array<V>, Collection<number, V> {
      * @param thisArg An object to which the this keyword can refer in the callbackfn function.
      * If thisArg is omitted, undefined is used as the this value.
      */
-    // @ts-ignore
+    every<S extends V>(predicate: (value: V, index: number, array: V[]) => value is S, thisArg?: any): this is S[];
+    every(callbackfn: (value: V, index: number, array: V[]) => unknown, thisArg?: any): boolean;
     every(callbackfn: (value: V, index: number, array: V[]) => unknown, thisArg?: any): boolean {
         return this.items.every(callbackfn, thisArg);
     }
